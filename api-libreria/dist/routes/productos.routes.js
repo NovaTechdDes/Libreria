c4736a3e49bf5c423093743dafb9c361029e02ba
Pelusa-not <agustinlorenzatto@outlook.com>
2026-04-08 11:01:44 -0300

    Borrando archivos y carpetas que no utilizo en github

--- api-libreria/dist/routes/productos.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const productos_controller_1 = require("../controllers/productos.controller");
-const router = (0, express_1.Router)();
-router.get("/", productos_controller_1.getProductos);
-router.get("/search/:texto", productos_controller_1.searchProductos);
-exports.default = router;
--- api-libreria/src/server.ts
+++ api-libreria/src/server.ts
@@ -2,21 +2,24 @@ import app from "./app";
 import dotenv from "dotenv";
 import fs from "fs";
 import https from "https";
+import path from "path";
 
-dotenv.config();
+dotenv.config({
+  path: path.join(__dirname, "../.env"),
+});
 
 const PORTLOCAL = process.env.PORT_LOCAL || 3000;
 const PORT = process.env.PORT || 3001;
 
-const options = {
-  key: fs.readFileSync("./cert/key.pem"),
-  cert: fs.readFileSync("./cert/cert.pem"),
-};
+// const options = {
+//   key: fs.readFileSync("./cert/key.pem"),
+//   cert: fs.readFileSync("./cert/cert.pem"),
+// };
 
 app.listen(PORTLOCAL, () => {
   console.log(`Servidor corriendo en puerto ${PORTLOCAL} como puerto local`);
 });
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT} como puerto https`);
-});
+// https.createServer(options, app).listen(PORT, () => {
+//   console.log(`Servidor corriendo en puerto ${PORT} como puerto https`);
+// });

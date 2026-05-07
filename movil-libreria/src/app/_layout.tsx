cae52a948dec3ff76b573c689af94ab992ca563f
Pelusa-not <agustinlorenzatto@outlook.com>
2026-05-07 19:17:01 -0300

    CAmbiando el estatus bar

--- movil-libreria/src/app/_layout.tsx
+++ movil-libreria/src/app/_layout.tsx
@@ -1,18 +1,23 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
-import Toast from "react-native-toast-message";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Toast from 'react-native-toast-message';
+import { useColorScheme } from 'nativewind';
 
-import "../../global.css";
+import '../../global.css';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const { colorScheme } = useColorScheme();
+
   return (
     <SafeAreaProvider>
       <SafeAreaView className="flex-1 bg-gray-50 dark:bg-slate-950">
         <QueryClientProvider client={queryClient}>
           <Stack>
+            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           </Stack>
         </QueryClientProvider>

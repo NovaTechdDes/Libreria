f98e84635ff8f9dcf6d2d5e9a7b137373ab7cf7e
Pelusa-not <agustinlorenzatto@outlook.com>
2026-06-09 10:55:15 -0300

    Sacnado rubros que no esten en la lista

--- web-libreria/src/helper/ordernarRubros.ts
+++ web-libreria/src/helper/ordernarRubros.ts
@@ -17,11 +17,11 @@ const ordenRubros = [
     'PROMOCIONES OFERTAS'
 ]
 
+const ordenMap = new Map(
+    ordenRubros.map((nombre, index) => [nombre, index])
+);
+
+
 export const ordenarRubros = (rubros: Rubro[]) => {
-    const rubrosOrdenados =  rubros.sort((a, b) => {
-        return (
-            ordenRubros.indexOf(a.nombre) - ordenRubros.indexOf(b.nombre)
-        )
-    })
-    return rubrosOrdenados
+    return rubros.filter((rubro) => ordenMap.has(rubro.nombre)).sort((a, b) => (ordenMap.get(a.nombre)! - ordenMap.get(b.nombre)!))
 }
